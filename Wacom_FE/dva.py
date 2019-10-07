26a9e2f1d7a5d57c0023e7e956ffdf5756b381fe
souvikchakraborty98 <souvik.chakraborty59@yahoo.com>
2019-10-07 19:52:14 +0530

    Interactive graph without call

--- Wacom_FE/dva.py
+++ Wacom_FE/dva.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import csv
+import subprocess
+import mplcursors
 
 x = []
 y = []
@@ -16,10 +18,10 @@ with open('fbs.csv','r') as csvfile:
 # plt.rcParams["figure.figsize"] = fig_size
 
 plt.subplot(1,2,1)
-plt.plot(x,y, label='xy plot')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('Coordinate data plot')
+plt.scatter(x,y,label='xy plot')
+plt.xlabel('x pixels')
+plt.ylabel('y pixels')
+plt.title('Coordinate data Scatter plot')
 plt.legend()
 
 x = []
@@ -31,11 +33,13 @@ with open('pbs.csv','r') as csvfile:
 
 plt.subplot(1,2,2)
 plt.plot(x, label='xy plot')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('Presssure data plot')
+plt.xlabel('x pixels')
+plt.ylabel('y pixels')
+plt.title('Presssure data 2D plot')
 plt.legend()
 mng=plt.get_current_fig_manager()
 mng.window.state("zoomed")
+mplcursors.cursor(hover=True)
 plt.show()
 
+#subprocess.call(["Wacom Feature Extractor.exe"])
